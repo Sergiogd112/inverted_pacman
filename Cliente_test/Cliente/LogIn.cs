6e9cddfe5508ed780a80280779a550f186d2bd24
Jauume <jaume.lopez.defez@estudiantat.upc.edu>
2023-03-21 16:19:52 +0100

    Añadir Cliente Text
    Log in y register

---
+++ Cliente_test/Cliente/LogIn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Version_1
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //No hace nada, dejar vacío
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e) //Al apretar el botón accept de Log In
+        {
+            string user = userBox.Text;
+            string password = passwordBox.Text;
+            string complete = "2/" + user + "/" + password;
+
+
+            MessageBox.Show(complete);
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            Register form = new Register(); // Crear una instancia del nuevo formulario
+            form.Show(); // Mostrar el nuevo formulario
+
+        }
+    }
+}
