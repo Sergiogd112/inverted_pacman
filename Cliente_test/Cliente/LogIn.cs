1e6171c0a707ec60aa17a1f6e5908ef0a14913ad
Joowww <72143824+Joowww@users.noreply.github.com>
2023-04-18 23:40:11 +0200

    Cambio visual

--- Cliente_test/Cliente/LogIn.cs
+++ Cliente_test/Cliente/LogIn.cs
@@ -19,6 +19,13 @@ namespace Version_1
     public partial class LogIn : Form
     {
         Socket server;
+        Thread atender;
+        delegate void Del_ParaGrid(string[] mensaje, int codigo);
+        delegate void Del_ParaDesconectar();
+        bool Conectado = false;
+        bool Conectado_Click = false;
+        bool Logeado = false;
+        bool changeALLOW = false;
         public LogIn()
         {
             InitializeComponent();
@@ -29,56 +36,351 @@ namespace Version_1
             //No hace nada, dejar vacío
         }
 
-        private void acceptButton_Click(object sender, EventArgs e) //Al apretar el botón accept de Log In
+        public void Desconectar()
         {
-
-            IPAddress address = IPAddress.Parse("192.168.56.102");
-            IPEndPoint ipep = new IPEndPoint(address, 9050);
-
-            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
+            if (changeALLOW)
             {
-                server.Connect(ipep);
-                this.BackColor = Color.Green;
-                MessageBox.Show("Conectado");
 
-                string user = userBox.Text;
-                string password = passwordBox.Text;
-                string message = "2/" + user + "/" + password;
-
-                byte[] msg = Encoding.ASCII.GetBytes(message);
-                server.Send(msg);
-                byte[] msg2 = new byte[80];
-                server.Receive(msg2);
-                message = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-                MessageBox.Show(message);
-                message = "0/";
-                msg = Encoding.ASCII.GetBytes(message);
-                server.Send(msg);
-                this.BackColor = Color.DimGray;
-                server.Shutdown(SocketShutdown.Both);
-                server.Close();
-                MessageBox.Show("Desconectado");
+                this.BackColor = Color.Gray;
+                userBox.Clear();
+                passwordBox.Clear();
+                connlbl.Text = " ";
+                dataGridView1.Rows.Clear();
+                changeALLOW = false;
             }
-            catch (SocketException ex)
-            {
-                MessageBox.Show("No me he podido conectar");
-                return;
-            }
-
-
-
-
-
 
 
         }
 
+        public void PonerEnGrid(string[] mensaje, int hack)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.ColumnHeadersVisible = true;
+            string c = Convert.ToString(hack);
+            dataGridView1.Rows.Add("Hay " + c + " usuario/s conectado/s");
+            int i;
+            for (i = 0; i < hack; i++)
+            {
+                dataGridView1.Rows.Add(mensaje[i]);
+            }
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+
+        private void AtenderServidor()
+        {
+            while (true)
+            {
+                // Recibimos mensaje del servidor
+                byte[] msg2 = new byte[100];
+                server.Receive(msg2);
+                string[] error_servidor = Encoding.ASCII.GetString(msg2).Split('\0');
+                if (error_servidor[0] == "")
+                {
+                    MessageBox.Show("Servidor en tareas de mantenimiento, vuelva a conectarse más tarde");
+                    Del_ParaDesconectar delegado = new Del_ParaDesconectar(Desconectar);
+                    passwordBox.Invoke(delegado, new object[] { });
+                    server.Shutdown(SocketShutdown.Both);
+                    server.Close();
+                    atender.Abort();
+                }
+
+                else
+                {
+                    string[] trozos = error_servidor[0].Split('/');
+                    int codigo = Convert.ToInt32(trozos[0]);
+                    switch (codigo)
+                    {
+                        case 0: //Resupesta a la desconexión
+                            string mensaje1 = trozos[2].Split('\0')[0];
+                            int hack = Convert.ToInt32(trozos[1]);
+                            if (hack == 1)
+                            {
+                                MessageBox.Show(mensaje1);
+                            }
+                            else
+                            {
+                                MessageBox.Show(mensaje1);
+                                Del_ParaDesconectar delegado = new Del_ParaDesconectar(Desconectar);
+                                passwordBox.Invoke(delegado, new object[] { });
+                                server.Shutdown(SocketShutdown.Both);
+                                server.Close();
+                                atender.Abort();
+
+                            }
+
+                            break;
+
+                        case 1: //Respuesta al registrar
+                            string mensaje = trozos[1].Split('\0')[0];
+                            MessageBox.Show(mensaje);
+                            break;
+                        case 2: //Respuesta al iniciar sesión
+                            string mensaje2 = trozos[2].Split('\0')[0];
+                            int hack1 = Convert.ToInt32(trozos[1]);
+                            if (hack1 == 0)
+                            {
+                                MessageBox.Show(mensaje2);
+                                Logeado = true;
+                            }
+                            else
+                            {
+                                MessageBox.Show(mensaje2);
+                            }
+                            break;
+                        case 3: //Respuesta a la consulta 1
+                            string mensaje3 = trozos[1].Split('\0')[0];
+                            MessageBox.Show(mensaje3);
+                            break;
+                        case 4: //Respuesta a la consulta 2
+                            string mensaje4 = trozos[1].Split('\0')[0];
+                            MessageBox.Show(mensaje4);
+                            break;
+                        case 5: //Respuesta a la consulta 3
+                            string mensaje5 = trozos[1].Split('\0')[0];
+                            MessageBox.Show(mensaje5);
+                            break;
+                        case 6:
+                            int hack6 = Convert.ToInt32(trozos[1]);
+                            if (hack6 == 0)
+                                MessageBox.Show("No hay usuarios connectados");
+                            else
+                            {
+                                string[] mensaje6 = new string[hack6];
+                                for (int i = 0; i < hack6; i++)
+                                {
+                                    mensaje6[i] = (trozos[i + 2].Split('\0')[0]);
+
+                                }
+
+                                Del_ParaGrid delegado = new Del_ParaGrid(PonerEnGrid);
+                                dataGridView1.Invoke(delegado, new object[] { mensaje6, hack6 });
+                                //dataGridView1.Columns.Add("Column",mensaje[0]);
+                                //connlbl.Text = mensaje;
+
+
+                            }
+                            break;
+                    }
+
+                }
+            }
+
+        }
+        private void acceptButton_Click(object sender, EventArgs e) //Al apretar el botón accept de Log In
+        {
+
+            if (Conectado)
+            {
+                if (!Logeado)
+                {
+                    if (userBox.Text != "" & passwordBox.Text != "")
+                    {
+                        string mensaje = "2/" + userBox.Text + "/" + passwordBox.Text;
+                        // Enviamos al servidor el nombre tecleado
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+                        changeALLOW = true;
+
+                        //Recibimos la respuesta del servidor
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Rellene nombre de usuario y contraseña por favor");
+                    }
+                }
+                else
+                    MessageBox.Show("Sesión ya iniciada");
+            }
+            else
+                MessageBox.Show("Inicie conexión");
+        }
+
         private void registerButton_Click(object sender, EventArgs e)
         {
-            Register form = new Register(); // Crear una instancia del nuevo formulario
-            form.Show(); // Mostrar el nuevo formulario
+            if (Conectado)
+            {
+                if (userBox.Text != "" & passwordBox.Text != "")
+                {
+                    string mensaje = "1/" + userBox.Text + "/" + passwordBox.Text;
+                    // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
 
+
+                }
+                else
+                {
+                    MessageBox.Show("Rellene nombre de usuario y contraseña por favor");
+                }
+            }
+            else
+                MessageBox.Show("Inicie conexión");
+
+        }
+
+        private void Mostrar_Contraseña_CheckedChanged(object sender, EventArgs e)
+        {
+            string text = passwordBox.Text;
+            if (Mostrar_Contraseña.Checked)
+            {
+                passwordBox.UseSystemPasswordChar = false;
+                passwordBox.Text = text;
+
+            }
+            else
+            {
+                passwordBox.UseSystemPasswordChar = true;
+                passwordBox.Text = text;
+            }
+        }
+
+        private void Conectar_Click(object sender, EventArgs e)
+        {
+            if (!Conectado_Click)
+            {
+                //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+                //al que deseamos conectarnos
+                IPAddress direc = IPAddress.Parse(ipBox.Text);
+                IPEndPoint ipep = new IPEndPoint(direc, 50058);
+
+
+                //Creamos el socket 
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    server.Connect(ipep);//Intentamos conectar el socket
+                    this.BackColor = Color.Green;
+                    MessageBox.Show("Conectado");
+                    Conectado = true;
+
+                }
+                catch (SocketException ex)
+                {
+                    //Si hay excepcion imprimimos error y salimos del programa con return 
+                    MessageBox.Show("No he podido conectar con el servidor");
+                    return;
+                }
+                Conectado_Click = true;
+            }
+            else
+                MessageBox.Show("Conexión ya establecida");
+
+
+            //pongo en marcha el thread que atendera los mensajes del servidor
+            ThreadStart ts = delegate { AtenderServidor(); };
+            atender = new Thread(ts);
+            atender.Start();
+
+        }
+
+        private void Desconectar_Click(object sender, EventArgs e)
+        {
+            if (Conectado)
+            {
+                if (Logeado)
+                {
+                    string mensaje = "0/" + userBox.Text + "/" + passwordBox.Text;
+                    // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+                    Conectado_Click = false;
+                    Conectado = false;
+                    Logeado = false;
+                    changeALLOW = true;
+                }
+                else
+                    MessageBox.Show("No hay ninguna sesión iniciada");
+
+            }
+            else
+            {
+                MessageBox.Show("No hay ninguna conexión no establecida");
+            }
+        }
+
+        private void Enviar_Consulta_Click(object sender, EventArgs e)
+        {
+            if (Conectado)
+            {
+                if (Logeado)
+                {
+                    if (Consulta1.Checked)
+                    {
+                        string mensaje = "3/" + userBox.Text + "/" + passwordBox.Text;
+                        // Enviamos al servidor el nombre tecleado
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+
+                        //Recibimos la respuesta del servidor
+
+                    }
+
+                    else if (Consulta2.Checked)
+                    {
+                        string mensaje = "4/" + userBox.Text + "/" + passwordBox.Text;
+                        // Enviamos al servidor el nombre tecleado
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+
+                        //Recibimos la respuesta del servidor
+
+                    }
+                    else if (Consulta3.Checked)
+                    {
+                        string mensaje = "5/" + userBox.Text + "/" + passwordBox.Text;
+                        // Enviamos al servidor el nombre tecleado
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+
+                        //Recibimos la respuesta del servidor
+
+                    }
+                    else
+                        MessageBox.Show("Ninguna consulta seleccionada");
+                }
+                else
+                    MessageBox.Show("Inicie sesión");
+            }
+            else if (!Conectado)
+
+                MessageBox.Show("Inicie conexión");
+        }
+
+        private void Desconectar_Btn_Click(object sender, EventArgs e)
+        {
+            if (Conectado)
+            {
+                if (Logeado)
+                {
+                    string mensaje = "0/" + userBox.Text + "/" + passwordBox.Text;
+                    // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+                    Conectado_Click = false;
+                    Conectado = false;
+                    Logeado = false;
+                    changeALLOW = true;
+                }
+                else
+                    MessageBox.Show("No hay ninguna sesión iniciada");
+
+            }
+            else
+            {
+                MessageBox.Show("No hay ninguna conexión no establecida");
+            }
+        }
+
+        private void LogIn_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Conectado)
+            {
+                MessageBox.Show("Por favor asegurese de desconectarse del servidor antes de cerrar la ventana");
+                e.Cancel = true;
+            }
         }
     }
 }
