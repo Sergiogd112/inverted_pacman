39b8cea8aca0e3a6257ea4f17d3d837880672d66
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2023-04-21 20:56:16 +0200

    update python script

--- cliente python/cliente.py
+++ cliente python/cliente.py
@@ -1,33 +1,52 @@
 import socket
 
+import threading
+
+
+def receive(s, name):
+    while True:
+        data = s.recv(1024)
+        if len(repr(data.decode()))!=0:
+            print(name, "Received", repr(data.decode()))
+
+
 def main():
-    ussr_pwd=[
-        "sergio*teo",
-        "Anna*teo",
-        "Carlos*teo",
+    name_pwd = [
+        ("sergio", "teo"),
+        ("Anna", "teo"),
+        ("Carlos", "teo"),
     ]
-    sockets=[socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(len(ussr_pwd))]
+    sockets = [
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(len(name_pwd))
+    ]
     # create a socket object
 
     # get local machine name
-    host = '192.168.56.102'
+    host = "192.168.56.102"
 
     port = 9050
 
     # connection to hostname on the port.
-    [s.connect((host, port))  for s in sockets]
+    [s.connect((host, port)) for s in sockets]
 
     # send a thank you message to the client.
-    message = "2/pepa*pig"
-    [s.sendall(message.encode())  for s in sockets]
+    [
+        s.sendall((name + "*" + pwd).encode())
+        for s, (name, pwd) in zip(sockets, name_pwd)
+    ]
 
     # receive data from the server
-    datas = [s.recv(1024)  for s in sockets]
+    threads = [
+        threading.Thread(target=receive, args=(s, name))
+        for s, (name, pwd) in zip(sockets, name_pwd)
+    ]
+    [thread.start() for thread in threads]
 
-    [print('Received', repr(data.decode())) for data in datas]
     input("Acabar:")
-    message="0/"
-    [s.sendall(message.encode())  for s in sockets]
+    [thread.kill() for thread in threads]
+    message = "0/"
+    [s.sendall(message.encode()) for s in sockets]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
