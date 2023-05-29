2b7d537348a5fa6fea23c48645a9a79619c69730
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2023-05-29 16:58:15 +0200

    Update MainGameManager.cs

--- Client_Unity/Assets/MainGameManager.cs
+++ Client_Unity/Assets/MainGameManager.cs
@@ -1,30 +1,37 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
-<<<<<<< Updated upstream
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-=======
->>>>>>> Stashed changes
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Diagnostics;
+
 
 [CreateAssetMenu(fileName = "MainGameManager", menuName = "ScriptableObjects/MainGameManager", order = 1)]
 public class MainGameManager : ScriptableObject
 {
     public Client client;
     public bool host = false;
-    public string[] nombres = new string[4];
-    public float[][] ppositions = new float[4][2];
-    public float[][] epositions = new float[4][2];
+    public string[] names = new string[4];
+    public float[,] ppositions = new float[4, 2];
+    public float[,] epositions = new float[4, 2];
     public int[] ids = new int[4];
     public int[] points = new int[4];
     public int[] health = new int[4];
-    public Thread atentder;
+    public Thread atender;
     public bool timeouts = false;
     public int spawn = 0;
     public float[] spawnpos = new float[2];
-    public List<string> lognames=new List<string>();
-    public List<int> logids=new List<int>();
-    public List<int> logcodes=new List<int>();
+    public List<string> lognames = new List<string>();
+    public List<int> logids = new List<int>();
+    public List<int> logcodes = new List<int>();
 
     public void AtenderPartida()
     {
@@ -32,8 +39,8 @@ public class MainGameManager : ScriptableObject
         while (true)
         {
             byte[] msg2 = new byte[1000];
-            this.server.Receive(msg2);
-            this.Consultas = true;
+            this.client.server.Receive(msg2);
+
             string[] error_servidor = Encoding.ASCII.GetString(msg2).Split('\x04');
             if (error_servidor[0] == "")
             {
@@ -51,7 +58,8 @@ public class MainGameManager : ScriptableObject
                 continue;
             }
             int scode = Convert.ToInt32(elements[1]);
-            switch(scode){
+            switch (scode)
+            {
                 case 0:
                     DecodeFull(elements[2]);
                     break;
@@ -59,13 +67,14 @@ public class MainGameManager : ScriptableObject
                     DecodePlayers(Convert.ToInt32(elements[2]), elements[3]);
                     break;
                 case 2:
-                    DecodeEnemy(Convert.ToInt32(elements[2]),elements[3]);
+                    DecodeEnemy(Convert.ToInt32(elements[2]), elements[3]);
                     break;
             }
         }
     }
     public void DecodeEnemy(int code, string data)
     {
+
         switch (code)
         {
             case 0:
@@ -74,8 +83,8 @@ public class MainGameManager : ScriptableObject
                 {
                     string[] properties = elements[i].Split('*');
                     ids[i] = Convert.ToInt32(properties[0]);
-                    epositions[i][0] = Convert.ToFloat(properties[1]);
-                    epositions[i][1] = Convert.ToFloat(properties[2]);
+                    epositions[i, 0] = float.Parse(properties[1]);
+                    epositions[i, 1] = float.Parse(properties[2]);
                 }
                 break;
             case 1:
@@ -83,15 +92,16 @@ public class MainGameManager : ScriptableObject
                 {
                     break;
                 }
-                string[] properties = data[i].Split('*');
-                spawn = Convert.ToInt32(properties[0]);
-                spawnpos[0] = Convert.ToFloat(properties[1]);
-                spawnpos[1] = Convert.ToFloat(properties[2]);
-                break;                
+                string[] values = data.Split('*');
+                spawn = Convert.ToInt32(values[0]);
+                spawnpos[0] = float.Parse(values[1]);
+                spawnpos[1] = float.Parse(values[2]);
+                break;
         }
     }
-        public void DecodePlayers(int code, string data)
+    public void DecodePlayers(int code, string data)
     {
+
         switch (code)
         {
             case 0:
@@ -99,11 +109,11 @@ public class MainGameManager : ScriptableObject
                 for (int i = 0; i < 4; i++)
                 {
                     string[] properties = elements[i].Split('*');
-                    ppositions[i][0] = Convert.ToFloat(properties[1]);
-                    ppositions[i][1] = Convert.ToFloat(properties[2]);
-                    points[i]=Convert.ToInt32(properties[3]);
-                    health[i]=Convert.ToInt32(properties[4]);
-                    names[i]=properties[0];
+                    ppositions[i, 0] = float.Parse(properties[1]);
+                    ppositions[i, 1] = float.Parse(properties[2]);
+                    points[i] = Convert.ToInt32(properties[3]);
+                    health[i] = Convert.ToInt32(properties[4]);
+                    names[i] = properties[0];
                 }
                 break;
             case 1:
@@ -111,37 +121,37 @@ public class MainGameManager : ScriptableObject
                 {
                     break;
                 }
-                string[] properties = data[i].Split('*');
+                string[] log1 = data.Split('*');
                 logcodes.Add(1);
-                lognames.Add(properties[1]);
-                logids.Add(properties[2]);
-                break;                
+                lognames.Add(log1[1]);
+                logids.Add(Convert.ToInt32(log1[2]));
+                break;
             case 2:
                 if (host)
                 {
                     break;
                 }
-                string[] properties = data[i].Split('*');
+                string[] log2 = data.Split('*');
                 logcodes.Add(3);
-                lognames.Add(properties[1]);
-                logids.Add(properties[2]);
-                break;                
+                lognames.Add(log2[1]);
+                logids.Add(Convert.ToInt32(log2[2]));
+                break;
             case 3:
                 if (host)
                 {
                     break;
                 }
-                string[] properties = data[i].Split('*');
+                string[] log3 = data.Split('*');
                 logcodes.Add(3);
-                lognames.Add(properties[1]);
-                logids.Add(properties[2]);
-                break;                
+                lognames.Add(log3[1]);
+                logids.Add(Convert.ToInt32(log3[2]));
+                break;
         }
     }
     public void DecodeFull(string s)
     {
-        string[] data=s.Split('|');
-        DecodeEnemy(0,data[1]);
-        DecodePlayers(0,data[1]);
+        string[] data = s.Split('|');
+        DecodeEnemy(0, data[1]);
+        DecodePlayers(0, data[1]);
     }
 }
