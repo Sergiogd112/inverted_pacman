ad50ad5e1a12381fc2b121f43915e0c2f02df38e
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2023-06-24 16:18:33 +0200

    Update peticiones.h

--- server/server/peticiones.h
+++ server/server/peticiones.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mysql/mysql.h>
-#include loggear.h
+#include logger.h
 
 char *get_partidas_string_by_name(MYSQL *conn, const char *name, int *string_length);
 
