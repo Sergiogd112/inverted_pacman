49a92837c019ab9663a28504111322aaf7e1c591
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2023-06-27 21:18:01 +0200

    Update partida.c

--- server/server/partida.c
+++ server/server/partida.c
@@ -297,7 +297,8 @@ void Atender_Cliente_Partida(Partida *partida, Nombre nombre, MYSQL *conn)
         request[ret] = '\0';
         snprintf(logmsg, 2000, "Conexion %s ha mandado: %s\n", nombre, request);
         logger(LOGINFO, logmsg);
-
+        printf("Continuamos?\n");
+        scanf("");
         // Extract codes from the request.
         char *p = strtok(request, "/");
 
